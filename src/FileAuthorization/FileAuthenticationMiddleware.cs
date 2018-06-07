062d8d4d17b4093a3d29d1f7e30d9a51b65039bd
回收前女友 <jbl-2011@163.com>
2018-06-07 13:31:11 +0800

    init project and add FileAuthenticationMiddleware

---
+++ src/FileAuthorization/FileAuthenticationMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace FileAuthorization
+{
+    public class FileAuthenticationMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public FileAuthenticationMiddleware(RequestDelegate next)
+        {
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+
+
+            await _next(context);
+        }
+    }
+}
